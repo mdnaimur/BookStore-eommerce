6bd1217e5fd31b281f040dc96dc1b8c28c1b9abe
Md Naimur Rahman <mdnaimurr@gmail.com>
2020-08-27 02:09:55 +0600

    Book store new project start

---
+++ src/main/java/com/mnr/bookstore/BookStoreEommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mnr.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreEommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreEommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnr/bookstore/BookStoreEommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnr.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreEommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
