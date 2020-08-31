9b1fb96cd47432cf8a45112edf33b8689c93edb6
Md Naimur Rahman <mdnaimurr@gmail.com>
2020-08-31 16:34:12 +0600

    user account and database connection have done

--- src/main/java/com/mnr/bookstore/controller/HomeController.java
+++ src/main/java/com/mnr/bookstore/controller/HomeController.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping("/")
 	public String index() {
-		
+
 		return "index";
+	}
+
+	@RequestMapping("/myAccount")
+	public String myAccount() {
+
+		return "myAccount";
 	}
 
 }
